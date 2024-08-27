03873aa1cf3d3c1c4ba1c17a9ee5a94316718bcf
May Nicochelli <mayaranicochelli@gmail.com>
2024-08-26 23:23:27 -0300

    [Pet Adoption Center] feat: Add methods to call the Pets Adoption API

--- pet-adoption-center-project/src/pages/Home/index.jsx
+++ pet-adoption-center-project/src/pages/Home/index.jsx
@@ -1,33 +1,61 @@
-import { useState } from "react";
-import "./style.css";
-import Trash from "../../assets/trash.svg";
+import { useEffect, useState, useRef } from 'react';
+import './style.css';
+import Trash from '../../assets/trash.svg';
+import api from '../../services/api';
 
 function Home() {
-  const pets = [
-    {
-      id: "123456",
-      name: "test",
-      age: 1,
-    },
-    {
-      id: "1234567",
-      name: "test2",
-      age: 2,
-    },
-  ];
+  const [pets, setPets] = useState([])
+
+  const inputName = useRef();
+  const inputAge = useRef();
+  const inputGender = useRef();
+  const inputSpecies = useRef();
+  const inputBreed = useRef();
+  const inputStatus = useRef();
+  const inputDescription = useRef();
+
+  async function getPets() {
+    const responseApi = await api.get('/pets')
+    setPets(responseApi.data)
+    console.log(pets)
+  }
+
+  async function createPets() {
+    await api.post('/pets', {
+      name: inputName.current.value,
+      age: Number(inputAge.current.value),
+      gender: inputGender.current.value,
+      species: inputSpecies.current.value,
+      breed: inputBreed.current.value,
+      status: inputStatus.current.value,
+      desc: inputDescription.current.value,
+    })
+
+    getPets()
+  }
+
+  async function deletePets(id) {
+    await api.delete(`/pets/${id}`)
+
+    getPets()
+  }
+
+  useEffect(() => {
+    getPets()
+  }, []);
 
   return (
     <div className="container">
       <form className="form-container">
         <h1>Register new Pet</h1>
-        <input name="name" placeholder="Pet Name" type="text" />
-        <input name="age" placeholder="Age" type="number" />
-        <input name="gender" placeholder="Gender" type="text" />
-        <input name="species" placeholder="Species" type="text" />
-        <input name="breed" placeholder="Breed" type="text" />
-        <input name="status" placeholder="Status" type="text" />
-        <input name="desc" placeholder="Description" type="text" />
-        <button type="button">Register</button>
+        <input name="name" placeholder="Pet Name" type="text" ref={inputName}/>
+        <input name="age" placeholder="Age" type="number" ref={inputAge}/>
+        <input name="gender" placeholder="Gender" type="text" ref={inputGender}/>
+        <input name="species" placeholder="Species" type="text" ref={inputSpecies}/>
+        <input name="breed" placeholder="Breed" type="text" ref={inputBreed}/>
+        <input name="status" placeholder="Status" type="text" ref={inputStatus}/>
+        <input name="desc" placeholder="Description" type="text" ref={inputDescription}/>
+        <button type="button" onClick={createPets}>Register</button>
       </form>
 
       {pets.map((pet) => (
@@ -41,7 +69,7 @@ function Home() {
             <p>status: <span>{pet.status}</span></p>
             <p>desc: <span>{pet.desc}</span></p>
           </div>
-          <button>
+          <button onClick={() => deletePets(pet.id)}>
             <img src={Trash} />
           </button>
         </div>
